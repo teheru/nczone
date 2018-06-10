65ef8f714d7704b71fa0300a90034483e48a05fd
TeHeRu33 <eru@new-chapter.eu>
2018-06-10 21:11:45 +0200

    added and changed permission language entries

--- language/en/permissions_nczone.php
+++ language/en/permissions_nczone.php
@@ -20,8 +20,13 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
     'ACP_CAT_ZONE' => 'nC Zone',
-	'ACL_M_ZONE_MANAGE_PLAYERS' => 'nC Zone manage players',
-	'ACL_M_ZONE_MANAGE_MAPS' => 'nC Zone manage maps',
-	'ACL_M_ZONE_CREATE_MAPS' => 'nC Zone create and delete maps',
-	'ACL_M_ZONE_MANAGE_CIVS' => 'nC Zone manage civilizations',
+	'ACL_M_ZONE_MANAGE_PLAYERS' => 'manage players',
+	'ACL_M_ZONE_MANAGE_MAPS' => 'manage maps',
+	'ACL_M_ZONE_CREATE_MAPS' => 'create and delete maps',
+	'ACL_M_ZONE_MANAGE_CIVS' => 'manage civilizations',
+
+	'ACL_U_ZONE_DRAW' => 'draw matches',
+	'ACL_U_ZONE_LOGIN' => 'login / logout',
+	'ACL_U_ZONE_VIEW_LOGIN' => 'can see logged in players',
+	'ACL_U_ZONE_VIEW_MATCHES' => 'can see matches',
 ));
