eb5fbb35fa04471af895fb7257064b11b7dca278
Zutatensuppe <oo.para@gmail.com>
2018-06-17 02:56:33 +0200

    adding simple php unit test as an example

---
+++ tests/unit/utility/number_util_test.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace eru\nczone\tests\zone;
+
+use eru\nczone\utility\number_util;
+use PHPUnit\Framework\TestCase;
+
+class number_util_test extends TestCase
+{
+    /**
+     * @dataProvider cmp_data_provider
+     */
+    public function test_cmp($num1, $num2, int $expected): void
+    {
+        $this->assertSame($expected, number_util::cmp($num1, $num2));
+    }
+
+    public function cmp_data_provider(): array
+    {
+        return [
+            [4, 5, $expected = 1],
+            [10, -4, $expected = -1],
+            [10, 10, $expected = 0],
+            [10, 19, $expected = 1],
+            [null, 0, $expected = 0],
+            ["bla", 0, $expected = 0],
+            ["A", "778", $expected = -1],
+        ];
+    }
+}
