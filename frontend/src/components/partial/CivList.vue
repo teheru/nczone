bec52f968271d7235771401e7e6f816c1ba25fb4
Marian Cepok <marian.cepok@gmail.com>
2018-11-03 09:56:04 +0100

    Fixed space before comma in civ list

--- frontend/src/components/partial/CivList.vue
+++ frontend/src/components/partial/CivList.vue
@@ -1,9 +1,7 @@
 <template>
     <span>
         <span v-for="(civ, idx) in list" :key="idx">
-            <span class="zone-match-civ">{{ $t(civ.title) }}
-                <span class="zone-match-civ-tooltip">{{ civ.multiplier }}</span>
-            </span>
+            <span class="zone-match-civ">{{ $t(civ.title) }}<span class="zone-match-civ-tooltip">{{ civ.multiplier }}</span></span>
             <span v-if="idx+1 < list.length">, </span>
         </span>
     </span>
