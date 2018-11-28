b6be624ecb11d45b3638742a37eda0959a902e18
Zutatensuppe <oo.para@gmail.com>
2018-11-28 02:11:09 +0100

    running clean_match queries in a transaction

--- zone/matches.php
+++ zone/matches.php
@@ -181,18 +181,24 @@ class matches {
         });
     }
 
+    /**
+     * @param int $match_id
+     * @throws \Throwable
+     */
     public function clean_match(int $match_id): void
     {
-        $team_ids = $this->get_match_team_ids($match_id);
+        $this->db->run_txn(function () use ($match_id) {
+            $team_ids = $this->get_match_team_ids($match_id);
+            $where_match_id = "`match_id` = {$match_id}";
+            $where_team_id = $this->db->sql_in_set('team_id', $team_ids);
 
-        $where_match_id = ' WHERE `match_id` = ' . $match_id;
-        $where_team_id = ' WHERE ' . $this->db->sql_in_set('team_id', $team_ids);
-        $this->db->sql_query('DELETE FROM ' . $this->db->match_players_table . $where_team_id);
-        $this->db->sql_query('DELETE FROM ' . $this->db->match_civs_table . $where_match_id);
-        $this->db->sql_query('DELETE FROM ' . $this->db->match_team_civs_table . $where_team_id);
-        $this->db->sql_query('DELETE FROM ' . $this->db->match_player_civs_table . $where_match_id);
-        $this->db->sql_query('DELETE FROM ' . $this->db->match_teams_table . $where_team_id);
-        $this->db->sql_query('DELETE FROM ' . $this->db->matches_table . $where_match_id);
+            $this->db->sql_query("DELETE FROM {$this->db->match_players_table} WHERE {$where_team_id};");
+            $this->db->sql_query("DELETE FROM {$this->db->match_civs_table} WHERE {$where_match_id};");
+            $this->db->sql_query("DELETE FROM {$this->db->match_team_civs_table} WHERE {$where_team_id};");
+            $this->db->sql_query("DELETE FROM {$this->db->match_player_civs_table} WHERE {$where_match_id};");
+            $this->db->sql_query("DELETE FROM {$this->db->match_teams_table} WHERE {$where_team_id};");
+            $this->db->sql_query("DELETE FROM {$this->db->matches_table} WHERE {$where_match_id};");
+        });
     }
 
     /**
