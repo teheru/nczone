29c0a16c9d126db1fd8f95d580d6f53603d7734a
Marian Cepok <marian.cepok@gmail.com>
2021-10-18 10:48:34 +0200

    Added lock timer

---
+++ frontend/src/view/components/LockTimer.vue
@@ -0,0 +1,21 @@
+<template>
+  <span class="zone-small">(<span v-html="lockLength"></span>)</span>
+</template>
+<script>
+import { renderTime } from '@/functions'
+
+export default {
+  name: 'nczone-lock-timer',
+  props: {
+    lockLengthSeconds: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    lockLength () {
+      return renderTime(this.lockLengthSeconds, false)
+    }
+  }
+}
+</script>
---
+++ frontend/src/view/components/LoginDrawArea.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="zone-actions" v-if="havePossibleActions">
+    <button class="zone-button" v-if="canLogin" @click="doLogin" v-t="'NCZONE_LOGIN'"></button>
+    <button class="zone-button" v-if="isLoggedIn" @click="doLogout" v-t="'NCZONE_LOGOUT'"></button>
+
+    <button class="zone-button" v-if="canDraw && !(canBlockDraw && drawBlocked)" @click="doDraw" v-t="'NCZONE_DRAW'">
+      <template v-if="drawBlocked">
+        &nbsp;<div class="fa fa-lock"></div>
+        &nbsp;<nczone-lock-timer v-if="drawBlocked" :lockLengthSeconds="blockSeconds" />
+      </template>
+    </button>
+    <button class="fa fa-unlock zone-button" v-if="canBlockDraw && drawBlocked" @click="doDrawUnblock"></button>
+    <button class="fa fa-lock zone-button" v-if="canBlockDraw" @click="doDrawBlock">
+      <span v-if="drawBlocked">&nbsp;<nczone-lock-timer :lockLengthSeconds="blockSeconds" /></span>
+    </button>
+  </div>
+</template>
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'nczone-login-draw-area',
+  data () {
+    return {
+      'blockSeconds': 0
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'canLogin',
+      'drawBlockedTime',
+      'canBlockDraw',
+      'canDraw',
+      'isLoggedIn',
+      'timer'
+    ]),
+    havePossibleActions () {
+      return this.canDraw || this.canLogin || this.isLoggedIn || this.canBlockDraw
+    },
+    drawBlocked () {
+      return this.blockSeconds > 0
+    }
+  },
+  methods: {
+    doLogin () {
+      this.getDrawBlockedTime()
+      this.login()
+    },
+    doLogout () {
+      this.logout()
+    },
+    doDrawBlock () {
+      this.drawBlock()
+    },
+    doDrawUnblock () {
+      this.drawUnblock()
+    },
+    doDraw () {
+      this.getDrawBlockedTime()
+      if (!this.drawBlocked) {
+        this.openDrawPreviewOverlay()
+      }
+    },
+    ...mapActions([
+      'login',
+      'logout',
+      'getDrawBlockedTime',
+      'drawBlock',
+      'drawUnblock',
+      'openDrawPreviewOverlay'
+    ]),
+    cb (now) {
+      this.blockSeconds = this.drawBlockedTime - now / 1000
+    },
+    start () {
+      this.timer.every(0.25, this.cb)
+    },
+    stop () {
+      this.timer.off(this.cb)
+    }
+  },
+  mounted () {
+    this.start()
+  },
+  beforeDestroy () {
+    this.stop()
+  }
+}
+</script>
